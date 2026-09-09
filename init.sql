40db883925f69b0780b15bf72a1b8a7b4238ff6f
Sum <ddonation45@gmail.com>
2026-09-09 19:35:34 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -24,4 +24,4 @@ VALUES
 ('Rahul Sharma', '10:00', 'Software Developer', 'B.Tech', 'rahul@gmail.com', '9876543211', 'Male'),
 ('Sara Ahmed', '11:30', 'Data Analyst', 'MCA', 'sara@gmail.com', '9876543212', 'Female'),
 ('Ayaan Malik', '14:00', 'Business Analyst', 'BBA', 'ayaan@gmail.com', '9876543213', 'Male'),
-('Aisha Khan', '16:30', 'HR Executive', 'BBA', 'aisha@gmail.com', '9876543214', 'Female');
+('Aisha riyaz', '16:30', 'HR Executive', 'BBA', 'aisha@gmail.com', '9876543214', 'Female');
